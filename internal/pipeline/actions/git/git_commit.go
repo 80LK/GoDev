96c8ed2132f619af4a546e5187cd7895842f9efb
80LK <80lkru@gmail.com>
2026-07-08 19:03:09 +0300

    implement action GitCommit

--- internal/pipeline/actions/git/git_commit.go
+++ internal/pipeline/actions/git/git_commit.go
@@ -1,6 +1,8 @@
 package actions
 
 import (
+	"fmt"
+
 	"github.com/80LK/godev/internal/pipeline/context"
 	"github.com/80LK/godev/internal/pipeline/patches"
 )
@@ -10,6 +12,27 @@ type GitCommit struct {
 	Value    string
 }
 
-func (g GitCommit) Plan(*context.Context) ([]patches.Patch, error) {
-	panic("unimplemented")
+func (g GitCommit) Plan(ctx *context.Context) ([]patches.Patch, error) {
+	if g.InputKey != "" {
+		if _v, ok := context.Get[string](ctx, g.Value); ok {
+			g.Value = _v
+		} else if _v, ok := context.Get[fmt.Stringer](ctx, g.Value); ok {
+			g.Value = _v.String()
+		}
+	}
+
+	if g.Value == "" {
+		return nil, fmt.Errorf("Can't get value for name commit")
+	}
+
+	return []patches.Patch{
+		patches.ShellPatch{
+			Command: "git",
+			Args:    []string{"add", "."},
+		},
+		patches.ShellPatch{
+			Command: "git",
+			Args:    []string{"commit", "-m", g.Value},
+		},
+	}, nil
 }
